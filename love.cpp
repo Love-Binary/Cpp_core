a6de2881a22e858e47619dd4ae42f2ef5af64cd9
Lovepreet Singh <200228969+Love-Binary@users.noreply.github.com>
2025-10-28 10:49:02 +0530

    Create love.cpp

---
+++ love.cpp
@@ -0,0 +1,71 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// Linked List Node
+struct node {
+    int info;
+    struct node* link;
+};
+struct node* start = NULL;
+
+// Function to create list with n nodes initially
+void createList()
+{
+    if (start == NULL) {
+        int n;
+        printf("\nEnter the number of nodes: ");
+        scanf("%d", &n);
+        if (n != 0) {
+            int data;
+            struct node* newnode;
+            struct node* temp;
+            newnode = malloc(sizeof(struct node));
+            newnode->link = NULL;
+            start = newnode;
+            temp = start;
+            printf("\nEnter number to"
+                   " be inserted : ");
+            scanf("%d", &data);
+            start->info = data;
+
+            for (int i = 2; i <= n; i++) {
+                newnode = malloc(sizeof(struct node));
+                newnode->link = NULL;
+                temp->link = newnode;
+                printf("\nEnter number to"
+                       " be inserted : ");
+                scanf("%d", &data);
+                newnode->info = data;
+                temp = temp->link;
+            }
+        }
+        printf("\nThe list is created\n");
+    }
+    else
+        printf("\nThe list is already created\n");
+}
+
+// Function to traverse the linked list
+void traverse()
+{
+    struct node* temp;
+
+    // List is empty
+    if (start == NULL)
+        printf("\nList is empty\n");
+
+    // Else print the LL
+    else {
+        temp = start;
+        while (temp != NULL) {
+            printf("Data = %d\n", temp->info);
+            temp = temp->link;
+        }
+    }
+}
+void main()
+{
+	createList();
+	traverse();
+	
+}
